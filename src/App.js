d40ea21fceeea1c083c265d140a17a776492d53c
uenobu106 <uenobu106@gmail.com>
2021-12-12 23:22:03 +0900

    ✅  add test components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Hoge from "./components/Hoge";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Hoge />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
