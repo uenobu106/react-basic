2f606929d9fb6d1a7990fce6ed29425120c4aede
uenobu106 <uenobu106@gmail.com>
2021-12-12 23:32:55 +0900

    ✅

---
+++ src/components/Hoge.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hoge = () => {
+  return (
+    <>
+      <h1>hoge h1</h1>
+      <h1>hoge h1</h1>
+      <h1>hoge h1</h1>
+    </>
+  );
+};
+
+export default Hoge;
